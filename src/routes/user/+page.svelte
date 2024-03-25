a75390ccb6d2dafabb59126eda83f2122f704ec3
Galexion <voltaire577@gmail.com>
2024-03-25 04:11:37 -0400

    Full Release

--- src/routes/user/+page.svelte
+++ src/routes/user/+page.svelte
@@ -4,6 +4,7 @@
 	let profileImage: string;
 	let webring_id: any;
 	let token: any;
+	let widgetvalue: string;
 	function getCookie(cname: string) {
 		let name = cname + '=';
 		let ca = document.cookie.split(';');
@@ -22,6 +23,7 @@
 	onMount(async () => {
 		webring_id = getCookie('webring_id');
 		token = getCookie('token');
+		widgetvalue = `<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" /><style>* {margin: 0;padding: 0;}.roboRingTransport {width:150px;height:50px;background: url("https://cdn.galexion.link/roboring/RoboNetTransport.gif");margin:0px;}a {color:gray;text-decoration: none;}@font-face {font-family: 'Material Symbols Outlined';font-style: normal;font-weight: 100 700;src: url(https://fonts.gstatic.com/s/materialsymbolsoutlined/v170/kJEhBvYX7BgnkSrUwT8OhrdQw4oELdPIeeII9v6oFsI.woff2) format('woff2');}.material-symbols-outlinedaa {text-align: center;font-variation-settings:'FILL' 0,'wght' 400,'GRAD' 0,'opsz' 24;font-size: 50px;width:30px;height:30px;margin-top:-8px;font-family: 'Material Symbols Outlined';font-weight: normal;font-style: normal;line-height: 1;letter-spacing: normal;text-transform: none;display: inline-block;white-space: nowrap;word-wrap: normal;direction: ltr;-moz-font-feature-settings: 'liga';-moz-osx-font-smoothing: grayscale;}</style><div class="roboRingTransport"><div style="height:18px;width:150px;"><a style="display: block;height: 100%;width: 100%;" href="https://roboring.galexion.link"></a></div><div><a href="https://roboring.galexion.link/webring/previous/${webring_id}"><span class="material-symbols-outlinedaa">skip_previous</span></a><a href="https://roboring.galexion.link/webring/next/${webring_id}" style="padding-left: 65px;"><span class="material-symbols-outlinedaa">skip_next</span></a></div></div>`;
 
 		if (webring_id && token) {
 		} else {
@@ -35,7 +37,7 @@
 		await fetch('/members', options)
 			.then((response) => response.json())
 			.then((response) => {
-				webring = response.find((element: any) => (element.webring_id == webring_id));
+				webring = response.find((element: any) => element.webring_id == webring_id);
 			})
 			.catch((err) => console.error(err));
 
@@ -98,10 +100,11 @@
 <div>
 	<div style="display:flex">
 		<span style="padding-right:2em;">
-			<h3 class="chivo-mono-sub">Site Widgets</h3>
+			<h3 class="chivo-mono-sub">Compact</h3>
+			<input value="{widgetvalue}" />
 		</span>
 		<span>
-			<h3 class="chivo-mono-sub">Coming Soon</h3>
+			<div>{@html widgetvalue}</div>
 		</span>
 	</div>
 </div>
